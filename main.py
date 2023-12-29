e1e2b4175515d944055492819d85fe3b22788636
Bryan Rodrigues <bryanrodrigues899@gmail.com>
2023-12-29 10:51:10 -0300

    ajustado funcao de jogar novamente na categoria desejada

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import PhotoImage
 import pandas as pd
 import random
 
+
 # Carregar o arquivo excel
 df = pd.read_excel('questions.xlsx')
 df2 = pd.read_excel('tipo_perguntas.xlsx') 
@@ -33,14 +34,14 @@ def display_types():
     
     questions_label.config(text="Qual tipo de pergunta? ")
     
-    option1_btn.config(text="Entretenimento", state=tk.NORMAL, command=lambda:selecionar_tipo_1(perguntas_tipo_1))
-    option2_btn.config(text="Artes", state=tk.NORMAL, command=lambda:selecionar_tipo_1(perguntas_tipo_2))
-    option3_btn.config(text="Ciencia", state=tk.NORMAL, command=lambda:selecionar_tipo_1(perguntas_tipo_3))
-    option4_btn.config(text="Esportes", state=tk.NORMAL, command=lambda:selecionar_tipo_1(perguntas_tipo_4))
+    option1_btn.config(text="Entretenimento", state=tk.NORMAL, command=lambda:(selecionar_tipo_1(perguntas_tipo_1), random.shuffle(perguntas_tipo_1)))
+    option2_btn.config(text="Artes", state=tk.NORMAL, command=lambda:(selecionar_tipo_1(perguntas_tipo_2),random.shuffle(perguntas_tipo_2)))
+    option3_btn.config(text="Ciencia", state=tk.NORMAL, command=lambda:(selecionar_tipo_1(perguntas_tipo_3),random.shuffle(perguntas_tipo_3)))
+    option4_btn.config(text="Esportes", state=tk.NORMAL, command=lambda:(selecionar_tipo_1(perguntas_tipo_4),random.shuffle(perguntas_tipo_4)))
 
 
 def selecionar_tipo_1(perguntas_tipo_1):
-
+    
     question, option1, option2, option3, option4, answer, tipo = perguntas_tipo_1[current_questions]
     questions_label.config(text=question)
 
@@ -55,32 +56,26 @@ def selecionar_tipo_1(perguntas_tipo_1):
 
 def check_answer(answer):
 
-
     if answer == currect_answer.get():
         show_result_correct()
-    
         
     else:
         show_result_wrong(current_questions)
-       
-    
-    # if current_questions < 1:
-    #     display_types()
-    # else:
-    #     show_result()
 
+
+def embaralha_questoes(perguntas_tipo_1):
+    random.shuffle(perguntas_tipo_1)
 
 
 def show_result_correct():
     current_questions+1
-    messagebox.showinfo("Resposta certa",f"Parabéns Resposta correta {current_questions}")
+    messagebox.showinfo("Resposta certa",f"Parabéns Resposta correta")
     option1_btn.config(state=tk.DISABLED)
     option2_btn.config(state=tk.DISABLED)
     option3_btn.config(state=tk.DISABLED)
     option4_btn.config(state=tk.DISABLED)
 
     play_again_btn.pack()
-
 
 
 def show_result_wrong(answer):
@@ -94,11 +89,10 @@ def show_result_wrong(answer):
     play_again_btn.pack()
     
 
+
 #funcao para jogar novamente 
 def play_again():
-
     random.shuffle(questions)
-
     option1_btn.config(state=tk.NORMAL)
     option2_btn.config(state=tk.NORMAL)
     option3_btn.config(state=tk.NORMAL)
@@ -120,13 +114,11 @@ text_color = "#333333"
 button_color = "#4CAF50"
 button_text_color = "#FFFFFF"
 
-
 janela.config(bg=background_color)
 janela.option_add('*Font', 'Arial')
 
 
 # Icon na tela
-
 app_icon =PhotoImage(file="perguntadoslogo.png")
 fator_reducao = 4
 app_icon = app_icon.subsample(fator_reducao, fator_reducao)
@@ -140,6 +132,7 @@ questions_label.pack(pady=20)
 
 
 currect_answer = tk.IntVar()
+
 
 option1_btn = tk.Button(janela, text="", width=30, bg=button_color, fg=button_text_color, state=tk.DISABLED, font=("Arial", 10, "bold"))
 option1_btn.pack(pady=10)
@@ -158,10 +151,7 @@ play_again_btn = tk.Button(janela,command=play_again, text="Jogar novamente", wi
 #play_again_btn.pack(pady=10)
 
 
-
 display_types()
-#random.shuffle(questions)
-#print(len(perguntas_tipo_1))
 
-#print(tipo_1_df)
+
 janela.mainloop()
